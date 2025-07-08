283fcfdb1ce4b1f9e4eacc683caf028a571031aa
HuyNLQ <huy.nguyen@ascension6.com.au>
2025-07-08 10:18:18 +0700

    Fix config

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,17 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
+                // CSS
                 'resources/css/app.css',
+                'resources/css/timesheets/app.css',
+                'resources/css/util/dataTables.dataTables.min.css',
+
+                // JS
                 'resources/js/app.js',
-                'resources/js/util/dataTables.min.js'
+                'resources/js/util/dataTables.min.js',
+                'resources/js/leave_requests/index.js',
+                'resources/js/timesheets/index.js',
+                'resources/js/timesheets/edit.js',
             ],
             refresh: true,
         }),
